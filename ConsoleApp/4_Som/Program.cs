405596eb53d664f42a01fa9da821fa3bdad9f1a3
maartendebeuf <maartendebeuf@hotmail.com>
2020-09-29 20:39:53 +0200

    4_Som
    De console sluit af en VisualStudio geeft een foutmelding weer

---
+++ ConsoleApp/4_Som/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Som
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Schrijf een consoleapplicatie die de som van twee ingegeven getallen berekent en de uitkomst toont op het scherm. Wat gebeurt er als je een letter ingeeft? Hoe vangen we dit op? Pas dit ook toe op je oplossing.
+
+            Console.WriteLine("Geef het eerste getal in: ");
+            int getal1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Geef het tweede getal in: ");
+            int getal2 = int.Parse(Console.ReadLine());
+
+            int som = getal1 + getal2;
+
+            Console.WriteLine("De som van beide getallen is: " + som);
+        }
+    }
+}
