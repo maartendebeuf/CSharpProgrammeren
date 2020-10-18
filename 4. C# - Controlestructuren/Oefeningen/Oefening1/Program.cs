e54022845e33ccff8d643644767584e4b7eb824b
maartendebeuf <maartendebeuf@hotmail.com>
2020-10-18 21:54:48 +0200

    update

---
+++ 4. C# - Controlestructuren/Oefeningen/Oefening1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vraag een leeftijd en afhankelijk van de waarde, toon onderstaande boodschap:
+            //*Jonger dan 18: "U bent nog niet volwassen."
+            //* Tussen 18 en 25: "U bent een jongvolwassene."
+            //* Ouder dan 25: "U bent volwassen."
+
+            Console.WriteLine("Geef uw leeftijd in.");
+            int leeftijd = int.Parse(Console.ReadLine());
+
+            if ( leeftijd < 18)
+            {
+                Console.WriteLine("U bent nog niet volwassen.");
+            }
+            else if (leeftijd >= 18 || leeftijd <= 25)
+            {
+                Console.WriteLine("U bent een jongvolwassene.");
+            }
+            else
+            {
+                Console.WriteLine("U bent volwassen.");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
