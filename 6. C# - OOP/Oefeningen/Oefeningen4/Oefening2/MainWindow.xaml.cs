76b8d8cc445079591eb643ae7c7f6a4971d366a9
maartendebeuf <maartendebeuf@hotmail.com>
2021-03-05 23:09:34 +0100

    oefening4.2

--- 6. C# - OOP/Oefeningen/Oefeningen4/Oefening2/MainWindow.xaml.cs
+++ 6. C# - OOP/Oefeningen/Oefeningen4/Oefening2/MainWindow.xaml.cs
@@ -31,16 +31,201 @@ namespace Oefening2
         //**Bedienden** en **kaderleden** krijgen een maandwedde uitbetaald en ontvangen maaltijdcheques. Het aantal maaltijdcheques komt overeen met het aantal gewerkte dagen.\
         //**Kaderleden** hebben recht op een bedrijfswagen. Van de kaderleden wordt bijgehouden welk type auto ze hebben.\
         //Maak gebruik van overerving bij het oplossen van deze oefening.
+        public List<Werknemer> Werknemers { get; set; } = new List<Werknemer>();
+
         public MainWindow()
         {
+            this.DataContext = this;
             InitializeComponent();
             Werknemer werknemer = new Werknemer("Maarten", "De Beuf");
-            Bediende bediende = new Bediende("Kim", "Stichelbout");
-            bediende.GepresteerdeDagenMaand = 5;
-            Arbeider arbeider = new Arbeider("Lyen", "De Beuf");
-            arbeider.GepresteerdeUrenMaand = 120;
-            Kaderlid kaderlid = new Kaderlid("Maarten", "De Beuf", "Volkswagen");
-            kaderlid.GepresteerdeDagenMaand = 6;
+             
+        }
+
+        private void btnToevoegen_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(tbxVoornaam.Text)|| !string.IsNullOrWhiteSpace(tbxNaam.Text))
+                {
+                    if ((bool)rbArbeider.IsChecked)
+                    {
+                        ArbeiderToevoegen();
+                    }
+                    else if ((bool)rbBediende.IsChecked)
+                    {
+                        BediendeToevoegen();
+                    }
+                    else if ((bool)rbKaderlid.IsChecked)
+                    {
+                        if (!string.IsNullOrWhiteSpace(tbxBedrijfswagen.Text))
+                        {
+                            KaderlidToevoegen();
+                        }               
+                    } 
+                    else
+                    {
+                        Foutmelding();
+                    }    
+                }
+                else
+                {
+                    Foutmelding();
+                }
+                dgWerknemers.Items.Refresh();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }   
+        }
+        private void dgWerknemers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            WerknemerInfo();
+        }
+        private void btnPrestatieToevoegen_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int aantal = int.Parse(tbxPrestatieToevoegen.Text);
+                PrestatieToevoegen(aantal);
+            }
+            catch (Exception)
+            {
+                Foutmelding();
+            }
+            tbxPrestatieToevoegen.Text = "";
+        }
+
+        private void ArbeiderToevoegen()
+        {
+            string voornaam = tbxVoornaam.Text;
+            string naam = tbxNaam.Text;
+                       
+            Arbeider arbeider = new Arbeider(voornaam, naam);
+            arbeider.GepresteerdeUrenMaand = 0;
+            WerknemerToevoegen(arbeider);
+        }
+        private void BediendeToevoegen()
+        {
+            string voornaam = tbxVoornaam.Text;
+            string naam = tbxNaam.Text;
+
+            Bediende bediende = new Bediende(voornaam, naam);
+            bediende.GepresteerdeDagenMaand = 0;
+            WerknemerToevoegen(bediende);
+        }
+        private void KaderlidToevoegen()
+        {
+            string voornaam = tbxVoornaam.Text;
+            string naam = tbxNaam.Text;
+            string bedrijfswagen = tbxBedrijfswagen.Text;
+            Kaderlid kaderlid = new Kaderlid(voornaam, naam, bedrijfswagen);
+            kaderlid.GepresteerdeDagenMaand = 0;
+            WerknemerToevoegen(kaderlid);
+        }
+        private void WerknemerToevoegen(Werknemer werknemer)
+        {
+            Werknemers.Add(werknemer);
+            int aantal = Werknemers.Count() - 1;
+            dgWerknemers.SelectedIndex = aantal;
+        }
+        private void WerknemerInfo()
+        {
+            Werknemer werknemer = Werknemers[dgWerknemers.SelectedIndex];
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("Voornaam: " + werknemer.Voornaam);
+            stringBuilder.AppendLine("Naam: " + werknemer.Naam);
+            try
+            {
+                Arbeider arbeider = (Arbeider)werknemer;
+                stringBuilder.AppendLine("Prestaties: " + arbeider.GepresteerdeUrenMaand);
+
+                lblTypeLoon.Content = "uren";
+
+                tbxVoornaam.Text = arbeider.Voornaam;
+                tbxNaam.Text = arbeider.Naam;
+                tbxBedrijfswagen.Text = "";
+                rbArbeider.IsChecked = true;
+            }
+            catch (Exception)
+            {
+
+            }
+            try
+            {
+                Bediende bediende = (Bediende)werknemer;                
+                stringBuilder.AppendLine("Prestaties: " + bediende.GepresteerdeDagenMaand);
+
+                lblTypeLoon.Content = "dagen";
+                
+                tbxVoornaam.Text = bediende.Voornaam;
+                tbxNaam.Text = bediende.Naam;
+                tbxBedrijfswagen.Text = "";
+                rbBediende.IsChecked = true;
+            }
+            catch (Exception)
+            {
+
+            }
+            try
+            {
+                Kaderlid kaderlid = (Kaderlid)werknemer;
+                Bedrijfswagen bedrijfswagen = kaderlid.BedrijfswagenKaderlid;
+                stringBuilder.AppendLine("Bedrijfswagen: " + bedrijfswagen.Type);
+                stringBuilder.AppendLine("Prestaties: " + kaderlid.GepresteerdeDagenMaand);
+
+                lblTypeLoon.Content = "dagen";
+
+                tbxVoornaam.Text = kaderlid.Voornaam;
+                tbxNaam.Text = kaderlid.Naam;
+                tbxBedrijfswagen.Text = bedrijfswagen.Type;
+                rbKaderlid.IsChecked = true;
+            }
+            catch (Exception)
+            {
+
+            }
+            lblWerknemerInfo.Content = stringBuilder.ToString();
+
+        }
+        private void PrestatieToevoegen(int aantal)
+        {
+            Werknemer werknemer = Werknemers[dgWerknemers.SelectedIndex];
+            
+            try
+            {
+                Arbeider arbeider = (Arbeider)werknemer;
+                arbeider.GepresteerdeUrenMaand = arbeider.GepresteerdeUrenMaand + aantal;
+            }
+            catch (Exception)
+            {
+
+            }
+            try
+            {
+                Bediende bediende = (Bediende)werknemer;
+                bediende.GepresteerdeDagenMaand = bediende.GepresteerdeDagenMaand + aantal;
+            }
+            catch (Exception)
+            {
+
+            }
+            try
+            {
+                Kaderlid kaderlid = (Kaderlid)werknemer;
+                kaderlid.GepresteerdeDagenMaand = kaderlid.GepresteerdeDagenMaand + aantal;
+            }
+            catch (Exception)
+            {
+
+            }
+            WerknemerInfo();
+        }
+        private void Foutmelding()
+        {
+            string foutmelding = "Gelieve alles in te vullen";
+            MessageBox.Show(foutmelding);
 
         }
     }
